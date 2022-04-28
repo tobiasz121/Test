b38fba69388ebf6a0a2959716ffc625c6fc8a014
tobiasz121 <78846581+tobiasz121@users.noreply.github.com>
2022-04-28 15:47:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,18 +22,17 @@ class FetchWrapper {
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded",
                 "User-Agent": "niagara",
-                "Host" : "login.microsoftonline.com"
+                "Host" : "login.microsoftonline.com",
+                "Origin": "http://localhost"
             },
-            body: JSON.stringify(body)
+            body: 'grant_type=client_credentials&scope=https%3A%2F%2Fgraph.microsoft.com%2F.default&client_secret=kVa1CtmyMS3vaR3DT~_Lw48whA.~7g~S6z&client_id=a9355f0d-def3-411a-8c33-6b4d6b27972f'
         }).then(response => response.json());
     }
 }
 
 const API = new FetchWrapper('https://login.microsoftonline.com')
 
-API.post('/{55e67ea6-2636-4be5-8eaa-0b5e6de9ed4f}/oauth2/v2.0/token', `grant_type=client_credentials&
-scope=https%3A%2F%2Fgraph.microsoft.com%2F.default&
-client_secret=kVa1CtmyMS3vaR3DT~_Lw48whA.~7g~S6z&
-client_id=a9355f0d-def3-411a-8c33-6b4d6b27972f`).then(data => {
+API.post('/{55e67ea6-2636-4be5-8eaa-0b5e6de9ed4f}/oauth2/v2.0/token', `grant_type=client_credentials&scope=https%3A%2F%2Fgraph.microsoft.com%2F.default&client_secret=kVa1CtmyMS3vaR3DT~_Lw48whA.~7g~S6z&client_id=a9355f0d-def3-411a-8c33-6b4d6b27972f`)
+.then(data => {
     console.log(data)
 })
