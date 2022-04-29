317a84b647dc0eabee58943c62f42ff2b851e9f1
tobiasz121 <78846581+tobiasz121@users.noreply.github.com>
2022-04-29 14:54:21 +0200

    push
    f

--- index.js
+++ index.js
@@ -30,22 +30,22 @@ class FetchWrapper {
     }
 }
 
-// const API = new FetchWrapper('https://login.microsoftonline.com')
+const API = new FetchWrapper('https://login.microsoftonline.com')
 
-// API.post('/{55e67ea6-2636-4be5-8eaa-0b5e6de9ed4f}/oauth2/v2.0/token', `grant_type=client_credentials&scope=https%3A%2F%2Fgraph.microsoft.com%2F.default&client_secret=kVa1CtmyMS3vaR3DT~_Lw48whA.~7g~S6z&client_id=a9355f0d-def3-411a-8c33-6b4d6b27972f`)
-// .then(data => {
-//     console.log(data)
-// })
+API.post('/{55e67ea6-2636-4be5-8eaa-0b5e6de9ed4f}/oauth2/v2.0/token', `grant_type=client_credentials&scope=https%3A%2F%2Fgraph.microsoft.com%2F.default&client_secret=kVa1CtmyMS3vaR3DT~_Lw48whA.~7g~S6z&client_id=a9355f0d-def3-411a-8c33-6b4d6b27972f`)
+.then(data => {
+    console.log(data)
+})
 
-fetch(`https://api.github.com/users/tobiasz121`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-        })
+// fetch(`https://api.github.com/users/tobiasz121`)
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data)
+//         })
 
-const req = new XMLHttpRequest();
+// const req = new XMLHttpRequest();
 
-req.open('POST', 'https://login.microsoftonline.com/{55e67ea6-2636-4be5-8eaa-0b5e6de9ed4f}/oauth2/v2.0/token', true);
-req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-req.send('grant_type=client_credentials&scope=https%3A%2F%2Fgraph.microsoft.com%2F.default&client_secret=kVa1CtmyMS3vaR3DT~_Lw48whA.~7g~S6z&client_id=a9355f0d-def3-411a-8c33-6b4d6b27972f')
-console.log(req.responseText)
+// req.open('POST', 'https://login.microsoftonline.com/{55e67ea6-2636-4be5-8eaa-0b5e6de9ed4f}/oauth2/v2.0/token', true);
+// req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+// req.send('grant_type=client_credentials&scope=https%3A%2F%2Fgraph.microsoft.com%2F.default&client_secret=kVa1CtmyMS3vaR3DT~_Lw48whA.~7g~S6z&client_id=a9355f0d-def3-411a-8c33-6b4d6b27972f')
+// console.log(req.responseText)
